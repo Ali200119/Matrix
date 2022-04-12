80ce148219d89e681fbe41de2ed43cc90794d995
Ali200119 <aliit@code.edu.az>
2022-04-12 17:43:38 +0400

    Start

---
+++ Matrix/Matrix World/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Matrix_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine($"Wake up, {name}...");
+
+            Console.Clear();
+
+            Console.WriteLine("The Matrix has you...");
+
+            Console.Clear();
+
+            Console.WriteLine("Follow the white rabbit.");
+
+            Console.Clear();
+
+            Console.WriteLine($"Knock, knock, {name}.");
+        }
+    }
+}
